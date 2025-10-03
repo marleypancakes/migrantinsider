ccdf06975a57e690959a2b1d1a4a2ec9de58e938
reyaw <reyaw@protonmail.com>
2025-10-03 16:21:38 -0400

    shorten page paths

--- gatsby-node.js
+++ gatsby-node.js
@@ -61,7 +61,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     // Template For blog-post
     posts.forEach(({node}) => {
 
-      node.url = `/${node.slug}/`;
+      node.url = `/${node.slug.slice(0,60)}/`;
 
       actions.createPage({
         // path: post.node.fields.slug.split('/').slice(2, -1).join('/') === '' ? '/' : `/${post.node.fields.slug.split('/').slice(2, -1).join('/')}`,
